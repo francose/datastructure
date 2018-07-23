ef8ef4bc26daf18000e9766c7201e0e7e80db255
francose <elmanoukdesign@gmail.com>
2018-07-22 20:33:34 -0400

    Data structures : Week 2 / Traversals

---
+++ Traversal.py
@@ -0,0 +1,80 @@
+"""
+Sadik Erisen
+Data Structure - week 2
+In-order , Pre-order and Post-order Traversal examples
+
+"""
+import random
+
+class Node:
+
+    def __init__(self, data):
+
+        self.left = None
+        self.right = None
+        self.data = data
+# Insert Node
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+# Print the Tree
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print( self.data),
+        if self.right:
+            self.right.PrintTree()
+
+# Inorder traversal
+# Left -> Root -> Right
+    def inorderTraversal(self, root):
+        res = []
+        if root:
+            res = self.inorderTraversal(root.left)
+            res.append(root.data)
+            res = res + self.inorderTraversal(root.right)
+        return res
+
+    # Preorder traversal
+# Root -> Left ->Right
+    def PreorderTraversal(self, root):
+        res = []
+        if root:
+            res.append(root.data)
+            res = res + self.PreorderTraversal(root.left)
+            res = res + self.PreorderTraversal(root.right)
+        return res
+
+# Postorder traversal
+# Left ->Right -> Root
+    def PostorderTraversal(self, root):
+        res = []
+        if root:
+            res = self.PostorderTraversal(root.left)
+            res = res + self.PostorderTraversal(root.right)
+            res.append(root.data)
+        return res
+
+#define a root
+root = Node(27)
+
+for i in range(0, 10):
+    rannum = random.randint(i, 30)
+    root.insert(rannum)
+
+
+print(root.inorderTraversal(root))
+print(root.PreorderTraversal(root))
+print(root.PostorderTraversal(root))
